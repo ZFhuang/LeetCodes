53ed0ebd98c53d1646ffef0ef7448bbbb87f5609
Zifeng <huangsnake@gmail.com>
2020-02-22 01:00:12 +0800

    Better 0003

--- 0003_Longest_Substring_Without_Repeating_Characters.cpp
+++ 0003_Longest_Substring_Without_Repeating_Characters.cpp
@@ -21,16 +21,20 @@
 
 class Solution {
 public:
+
+    //字符数组标记法,65.7%
+    //遇到重复字符时将重复处之前的字符记录删去
     int lengthOfLongestSubstring(string s) {
         int count=0;
         int max=0;
         int tail=0; //尾部标记
         int hashmap[128];
         //初始化赋值用memset，且memset只能按照字节赋值，也就是建议赋值0和-1
-        //数组初始值是0
+        //数组默认初始值是0
         //若初始化时列出的元素比较少，则只会初始化前面的部分
         memset(hashmap,-1,sizeof(int)*128);
         for(int i=0;i<s.size();i++){
+            //只需要其最后出现的位置即可，不用记录为链表，会徒增麻烦
             if(hashmap[(int)(s[i])]<tail){
                 //记录元素的最后出现位置
                 hashmap[(int)(s[i])]=i;
@@ -49,5 +53,30 @@ public:
             max=count;
         }
         return max;
+    }
+
+    //优化筛选算法,92.98%
+    //利用最后一个重复串之后的串就可能是最长无重复串的特性
+    int lengthOfLongestSubstring(string s) {
+        vector<int>a(128, -1);
+        //j是已出现的最后一个出现重复的字符的串中位置
+        int j = 0, result = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            //相当于用i和j将问题拆分为子串来处理
+            //当目前字符在记录中出现时
+            if (a[s[i]] != -1) {
+                //中途结果取当前结果与当前位置减j的最大值
+                //即求子串中的剩余串长是否比之前的最大串长长
+                result = max(result, i - j);
+                //将这个字符出现前出现的字符的记录重置为无
+                for (; j <= a[s[i]]; ++j)
+                    a[s[j]] = -1;
+            }
+            //记录此字符在字符串中的位置
+            a[s[i]] = i;
+        }
+        //最终结果是中途结果（之前最大的串长）与字符串长度减j（最终的剩余串长）中的最大值
+        result = max(result, int(s.length()) - j);
+        return result;
     }
 };
---
+++ 0004_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,59 @@
+﻿/*
+给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+你可以假设 nums1 和 nums2 不会同时为空。
+
+示例 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+则中位数是 2.0
+示例 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+则中位数是 (2 + 3)/2 = 2.5
+*/
+
+#include<vector>
+#include<iostream>
+using namespace std;
+
+int main() {
+    Solution s;
+    cout << s.findMedianSortedArrays({ 1,2 }, { 3,4 });
+}
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        auto i = nums1.begin();
+        auto j = nums2.begin();
+        int mid = (nums1.size() + nums2.size()) / 2 + 1;
+        int odd = 0;
+        if ((nums1.size() + nums2.size()) % 2 == 1)
+            odd = 1;
+        int cursor = 0;
+        int a, b;
+        while (i != nums1.end() || j != nums2.end()) {
+            if (i != nums1.end() && (j == nums2.end() || *i <= *j)) {
+                a = b; b = *i;
+                ++i;
+            }
+            else if (j != nums2.end() && (i == nums1.end() || *j < *i)) {
+                a = b; b = *j;
+                ++j;
+            }
+            if (++cursor == mid)
+                break;
+        }
+        if (odd)
+            return b;
+        else
+            return (a + b) / 2;
+    }
+};
