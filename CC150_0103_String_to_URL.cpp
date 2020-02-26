dfd8ffa38c023b5ccc1bd07d512955749bb394dc
Zifeng <huangsnake@gmail.com>
2020-02-26 12:40:11 +0800

    0103,0104

--- CC150_0103_String_to_URL.cpp
+++ CC150_0103_String_to_URL.cpp
@@ -20,7 +20,43 @@ URL化。编写一种方法，将字符串中的空格全部替换为%20。假定该字符串尾部有足够的空间
 
 class Solution {
 public:
+    //传统方法,82.3%
+    //将改变后的字符串输入到新的数组中,记得结尾要附一个结束符
     string replaceSpaces(string S, int length) {
+        //长度为目标字符串+1,省点内存
+        char* newString = new char[S.size() + 1];
+        int cursor = 0;
+        for (int i = 0; i < length; i++) {
+            if (S[i] == ' ') {
+                newString[cursor] = '%';
+                newString[cursor + 1] = '2';
+                newString[cursor + 2] = '0';
+                cursor += 2;
+            }
+            else {
+                newString[cursor] = S[i];
+            }
+            cursor++;
+        }
+        //结束符
+        newString[cursor] = '\0';
+        //string的指针构造
+        //string(const char * s, size_type n)
+        return string(newString, S.size() + 1);
+    }
 
+    //极致优化,100%
+    //关闭流同步+使用字符串拼接
+    string replaceSpaces(string S, int length) {
+        //由于cin，cout与stdin总是保持同步的，所以有很大性能损失
+        //此语句关闭流的同步加快cin和cout读取
+        ios::sync_with_stdio(false);
+        string str = "";
+        for (int i = 0; i < length; i++) {
+            //使用字符串拼接+=号的效率是最高的
+            if (S[i] == ' ') str += "%20";
+            else str += S[i];
+        }
+        return str;
     }
 };
---
+++ CC150_0104_Palindrome_Permutation.cpp
@@ -0,0 +1,50 @@
+﻿/*
+给定一个字符串，编写一个函数判定其是否为某个回文串的排列之一。
+
+回文串是指正反两个方向都一样的单词或短语。排列是指字母的重新排列。
+
+回文串不一定是字典当中的单词。
+
+示例1：
+
+输入："tactcoa"
+输出：true（排列有"tacocat"、"atcocta"，等等）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-permutation-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    //奇偶计数法,100%
+    //字符串为偶数时，必然不能出现奇数个字符
+    //字符串为奇数时，可以出现一次奇数字符
+    bool canPermutePalindrome(string s) {
+        int valid[128];
+        memset(valid, 0, sizeof(int) * 128);
+        //计数
+        for (auto c : s) {
+            valid[c] += 1;
+        }
+        int odds = 0;
+        int sOdds = s.size() % 2;
+        for (int i = 0; i < 128; i++) {
+            //奇数个字符
+            if (valid[i] % 2 == 1) {
+                odds++;
+            }
+            if (sOdds) {
+                //字符串奇数
+                if (odds > 1)
+                    return false;
+            }
+            else {
+                //字符串偶数
+                if (odds > 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
