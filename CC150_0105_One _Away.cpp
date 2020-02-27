034d7e0eb026b000f185ec2e2e6ea25111f8f7bd
Zifeng <huangsnake@gmail.com>
2020-02-27 14:42:48 +0800

    05,06

--- CC150_0105_One _Away.cpp
+++ CC150_0105_One _Away.cpp
@@ -25,5 +25,104 @@ second = "pal"
 
 class Solution {
 public:
+    //传统的指针同位扫描,51.5%
+    //即允许一个字符的出错，长度不同时指针往后错一位
+    bool oneEditAway(string first, string second) {
+        //注意size返回值是size_t，是无符号数
+        //因此需要+1比较
+        //当长度差距在两字符以上则false，因为此时无法通过一次编辑来改正
+        if ((first.size() + 1 > (second.size() + 2)) || (first.size() + 1 < (second.size())))
+            return false;
 
+        int cur1 = 0, cur2 = 0;
+        bool flag = false;
+        while (cur1 < first.size() && cur2 < second.size()) {
+            //当前位是否相同
+            if (first[cur1] != second[cur2]) {
+                //出现过一次错误就false
+                if (flag)
+                    return false;
+                else {
+                    flag = true;
+                    //替换，比较下一位是否相同或没有下一位了，相同才能继续，否则false
+                    if (first.size() == second.size()) {
+                        if (((cur1 + 1) != first.size()) && first[cur1 + 1] != second[cur2 + 1])
+                            return false;
+                    }
+                    //插入或删除，比较长的那一边的下一位是否与短的当前位相同，相同才继续
+                    else if (first.size() > second.size()) {
+                        if (((cur1 + 1) != first.size()) && first[cur1 + 1] != second[cur2])
+                            return false;
+                        else {
+                            ++cur1;
+                            continue;
+                        }
+                    }
+                    else if (first.size() < second.size()) {
+                        if (((cur2 + 1) != second.size()) && second[cur2 + 1] != first[cur1])
+                            return false;
+                        else {
+                            ++cur2;
+                            continue;
+                        }
+                    }
+                }
+            }
+            //移动指针
+            ++cur1;
+            ++cur2;
+        }
+        return true;
+    }
+
+    //在一次检测中判断三种情况,89.1%
+    //提前通过交换固定住长串和短串相对位置，实现了代码复用并减少了判断次数和复杂性
+    bool oneEditAway(string first, string second) {
+        int len1 = first.size();
+        int len2 = second.size();
+
+        //判断各自长度，将长的字符串放到second的位置
+        //长度也要记得交换
+        //这一步将插入与删除统一了，因为删除交换后相当于对方的插入
+        if (len1 > len2) {
+            string t = first;
+            first = second;
+            second = t;
+
+            int len = len1;
+            len1 = len2;
+            len2 = len;
+        }
+
+        //当长的字符串大于短的超过1，说明一次编辑无法实现
+        if (len2 - len1 > 1) {
+            return false;
+        }
+
+        //错误标记，最多只能发生一次错误所以用bool
+        bool state = false;
+        //主循环，循环初始化两个指针而循环短字符串到头时结束
+        //循环时固定增加的时长字符串的指针因为插入必然发生在长字符串中
+        for (int i1 = 0, i2 = 0; i1 < len1; i2++) {
+            //发生同字符时
+            if (first[i1] != second[i2]) {
+                if (state) {
+                    return false;
+                }
+                else {
+                    state = true;
+                    //原本是花哨的写法，这里我加了些括号
+                    //其实就是若长串比较长(长度不是相等)，则短串指针暂停前移一循环，标志插入或删除了
+                    //若长度相等，则正常前移，标志发生的是替换
+                    //而对于下一位的正确与否交予下一个循环负责
+                    i1 += ((len2 - len1) == 1 ? 0 : 1);
+                }
+            }
+            //都相同时，默认也让短串前进
+            else {
+                i1++;
+            }
+        }
+        return true;
+    }
 };
---
+++ CC150_0106_Compress_String.cpp
@@ -0,0 +1,94 @@
+﻿/*
+字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。若“压缩”后的字符串没有变短，则返回原先的字符串。你可以假设字符串中只包含大小写英文字母（a至z）。
+
+示例1:
+
+ 输入："aabcccccaaa"
+ 输出："a2b1c5a3"
+示例2:
+
+ 输入："abbccd"
+ 输出："abbccd"
+ 解释："abbccd"压缩后为"a1b2c2d1"，比原字符串长度更长。
+提示：
+
+字符串长度在[0, 50000]范围内。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/compress-string-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+	//传统的读取计数压入,90.1%
+	//先压入首次出现的字符，再计数，然后压入最终数字，进入下次循环
+	string compressString(string S) {
+		//长度小于3的字符串必然无法压缩，直接返回
+		if (S.size() < 3) {
+			return S;
+		}
+
+		int old = S.size();
+		string retS;
+		char last;
+		int count = 1;
+		//第一次压入
+		last = S[0];
+		retS += last;
+		for (int i = 1; i < old; ++i) {
+			//相同时技术
+			if (last == S[i]) {
+				++count;
+			}
+			else {
+				//用C11以后的自带函数to_string将数字转为字符串
+				//to_string效率低于sprintf但是也很高
+				retS += to_string(count);
+				count = 1;
+				last = S[i];
+				retS += last;
+			}
+		}
+		retS += to_string(count);
+		//最后比较长度并返回
+		if (retS.size() < old) {
+			return retS;
+		}
+		else {
+			return S;
+		}
+	}
+
+	//优化了压入字符串速度的写法,100%
+	//用字符串模板流ostringstream加速构造
+	string compressString(string S) {
+		//LeetCode常用的加速读写语句
+		//Lambda表达式，先关闭cincout同步，使cincout不经过缓冲区加速
+		//接触cin与cout的绑定
+		//这样写是加速了LeetCode自身评审时的速度
+		static auto speedup = []() {ios::sync_with_stdio(false); cin.tie(nullptr); return nullptr; }();
+		//直接返回
+		if (S == "") return "";
+		char last = S[0];
+		int nCount = 1;
+		string str = "";
+		//字符串模板类，会动态分配内存
+		//动态申请适合大小的内存，既满足了内存节省又满足了空间局部性
+		ostringstream ss;
+		//循环扫描字符串
+		for (int i = 1; i < S.size(); ++i) {
+			//重复字符时
+			if (S[i] == last) { ++nCount; continue; }
+			//将内容输入ss
+			ss << last << nCount;
+			nCount = 1;
+			last = S[i];
+		}
+		ss << last << nCount;
+		//最后从ss中得到所需的字符串
+		str = ss.str();
+		//返回较短者
+		return str.size() < S.size() ? str : S;
+	}
+};
